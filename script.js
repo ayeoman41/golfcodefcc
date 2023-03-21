9fecfc8580c65349dfbec9e8a057c4a60bdee709
Beez <ayeoman41@gmail.com>
2023-03-21 14:17:34 -0400

    console.log added with par,strokes

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const names = ["Hole in one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey"
 function golfScore(par, strokes) {
         if (strokes == 1) {
             return names[0]
-        }else if (strokes <= par - 2){
+        }else if (strokes <= par - 2) {
             return names[1]
         }else if (strokes == par - 1) {
             return names[2]
@@ -15,10 +15,8 @@ function golfScore(par, strokes) {
             return names[5]
         }else if(strokes >= par +3) {
             return names[6]
-        }else 
-            return "Change me"
     }
-            
+}
 
          
-golfScore(1,1);
+console.log(golfScore(5,4));
