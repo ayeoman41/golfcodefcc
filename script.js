e409df3df2ea81b67b0ed36a96f527d4fd160080
Beez <ayeoman41@gmail.com>
2023-03-21 14:06:58 -0400

    golf code updated

--- script.js
+++ script.js
@@ -5,7 +5,20 @@ function golfScore(par, strokes) {
             return names[0]
         }else if (strokes <= par - 2){
             return names[1]
-        }else if (strokes <= -1) {
+        }else if (strokes <= par - 1) {
             return names[2]
-        }else if 
+        }else if (strokes == par) {
+            return names[3]
+        }else if (strokes == par + 1) {
+            return names[3]
+        }else if (strokes == par + 2) {
+            return names[4]
+        }else if(strokes >= par +3) {
+            return names[5]
+        }else 
+            return "Change me"
+
+            }
+
          
+golfScore(1,1);
