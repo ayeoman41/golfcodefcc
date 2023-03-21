2e88ed80b680a24a101c57ab1895ff728d01def7
Beez <ayeoman41@gmail.com>
2023-03-21 13:59:40 -0400

    golfScore function added w/ if else

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-const names = ["Hole in one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"]
+const names = ["Hole in one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+        if (strokes == 1) {
+            return names[0]
+        }else if (strokes <= par - 2){
+            return names[1]
+        }else if (strokes <= -1) {
+            return names[2]
+        }else if 
+         
